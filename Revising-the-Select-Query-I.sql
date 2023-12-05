9a5f523f98dba7c9a2e5e72e3a100d6acf138747
Ziad Salah <ziadsalah2003@gmail.com>
2023-12-06 01:46:56 +0200

    Update Revising-the-Select-Query-I

---
+++ Revising-the-Select-Query-I.sql
@@ -0,0 +1,4 @@
+--Problem Link:https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+--Solution Link:https://www.hackerrank.com/challenges/revising-the-select-query/submissions/code/358953785
+select * from city
+where countrycode ='USA' and population >100000;
