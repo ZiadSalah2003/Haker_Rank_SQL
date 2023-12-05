6ec43dd4b3ee80326bfaecfc2be0da0a2ee4c8a8
Ziad Salah <ziadsalah2003@gmail.com>
2023-12-06 01:57:28 +0200

    Solve Revising-the-Select-Query-II

---
+++ Revising-the-Select-Query-II.sql
@@ -0,0 +1,5 @@
+--Problem Link:https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+--Solution Link:https://www.hackerrank.com/challenges/revising-the-select-query-2/submissions/code/359281233
+select name 
+from city
+where countrycode ='USA'and population >=120000
