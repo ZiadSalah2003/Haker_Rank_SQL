7c7e404bfc5912ab5041db844fd8a155621e6324
Ziad Salah <ziadsalah2003@gmail.com>
2023-12-06 02:02:33 +0200

    Solve Select All

---
+++ Select-All.sql
@@ -0,0 +1,3 @@
+--Problem Link:https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+--Solution Link:https://www.hackerrank.com/challenges/select-all-sql/submissions/code/359281405
+select * from city
