491b4f455526f4afff0be1abb40de118fbf3ca72
Ziad Salah <ziadsalah2003@gmail.com>
2023-12-06 02:14:15 +0200

    Solve Problem Select-By-ID

---
+++ Select-By-ID.sql
@@ -0,0 +1,5 @@
+--Problem Link:https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+--Solution Link:https://www.hackerrank.com/challenges/select-by-id/submissions
+select * 
+from city
+where id=1661
