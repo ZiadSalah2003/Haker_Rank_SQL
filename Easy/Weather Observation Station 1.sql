07446069c19c31d18fd941e8e568300f0d338790
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-07 04:33:40 +0200

    Add Weather Observation Station 1 SQL solution

---
+++ Easy/Weather Observation Station 1.sql
@@ -0,0 +1,4 @@
+--Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+--Solution Link : https://www.hackerrank.com/challenges/weather-observation-station-1/submissions/code/363764209
+select city,state 
+from station
