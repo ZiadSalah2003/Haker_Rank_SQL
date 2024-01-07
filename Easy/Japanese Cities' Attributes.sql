c42e04ef72bacbf303fe00673f6f0d216f3703b2
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-07 04:28:11 +0200

    Add Japanese Cities' Attributes SQL file

---
+++ Easy/Japanese Cities' Attributes.sql
@@ -0,0 +1,4 @@
+--Problem Link : https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+--Solution Link : https://www.hackerrank.com/challenges/japanese-cities-attributes/submissions/code/363763844
+select * from city
+where COUNTRYCODE = JPN;
