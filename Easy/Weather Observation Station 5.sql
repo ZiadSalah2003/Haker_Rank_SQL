95b2a53e3471935a285eeff6373f64f86f337c13
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-06-07 16:28:32 +0300

    Add SQL queries to retrieve shortest and longest city names from the station table

---
+++ Easy/Weather Observation Station 5.sql
@@ -0,0 +1,10 @@
+--Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=true
+select  city, length(city) 
+from station 
+order by length(city) ,city
+limit 1;
+
+select city, length(city) 
+from station 
+order by length(city) desc, city
+limit 1;
