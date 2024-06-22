562d1d774b5dcf0da75ff174f2838bf8c043a658
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-06-22 04:07:53 +0300

    feat: Add SQL query to retrieve cities not starting with vowels

---
+++ Easy/Weather Observation Station 9.sql
@@ -0,0 +1,4 @@
+--Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true
+select distinct city
+from station 
+where city not like 'A%' and city not like 'E%' and city not like 'I%' and city not like 'O%' and city not like 'U%'
